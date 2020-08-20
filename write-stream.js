311d2f20a7580c22272c697d367ad84c1e2bc041
Jiawei <liujiaweidreamer@gmail.com>
2020-08-20 11:27:15 +0800

    "write" method return a boolean, usually used with "drain" event

--- write-stream.js
+++ write-stream.js
@@ -2,9 +2,21 @@ const fs = require("fs");
 
 const writeStream = fs.createWriteStream('./streamIO.txt');
 
-for (let i = 0; i < 10000; i++) {
-  writeStream.write(`这是第 ${i} 次插入数据\n`);
+let falseCount = 0;
+
+for (let i = 0; i < 1000000; i++) {
+  const ok = writeStream.write(`这是第 ${i} 次插入数据\n`);
+//  console.log(ok);
+
+  if (ok) {
+    console.log('不够喝');
+  } else {
+     console.log('太快了');
+    falseCount++;
+  }
 }
+
 // writeStream.write('666\n');
 
 console.log('done');
+console.log(falseCount);
